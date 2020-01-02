be3347e0e99a9de1c20ce989cd9ff8a801c776da
bwojtal <bartekwojtal1207@gmail.com>
2020-01-02 18:24:32 +0100

    navbar

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-
-import stlyes from './Layout.module.scss';
-import Navbar from '../../components/Navbar/Navbar'
+import styles from './Layout.module.scss';
+import Navbar from '../../components/Navbar/Navbar';
+import Logo from './header1.jpg';
 
 class Layout extends Component {
 
     render() {
         return (
-         <div>
-                <Navbar stylesApp={stlyes}/>
+         <div  className={styles.HeaderImg} style={{ 'backgroundImage': 'linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url("' + Logo + '")'}}>
+                <Navbar stylesApp={styles}/>
                 {/*<div className={stlyes.Container}>*/}
                 {/*    {this.props.children}*/}
                 {/*</div>*/}
