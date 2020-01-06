fb0ef01a99ec187d7538cfdcdbb596be88783c20
bwojtal <bartekwojtal1207@gmail.com>
2020-01-06 14:52:46 +0100

    numbers section

--- src/components/Numbers/NumberItems/NumberItems.js
+++ src/components/Numbers/NumberItems/NumberItems.js
@@ -1,43 +1,66 @@
-import React from "react";
+import React, {Component} from "react";
 
-const NumberItems = () => {
-    const numbersValue = [
-        {
-            value: '147',
-            title: 'instutycji finansowych'
-        }, {
-            value: '6,5',
-            title: 'mln realnych użytkowników portalu'
-        }, {
-            value: '968',
-            title: 'produktów finansowych'
+class NumberItems extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            boxes : [
+                {
+                    currentValue: 0,
+                    value: '147',
+                    title: 'instutycji finansowych'
+                }, {
+                    currentValue: 0,
+                    value: '6.5',
+                    title: 'mln realnych użytkowników portalu'
+                }, {
+                    currentValue: 0,
+                    value: '968',
+                    title: 'produktów finansowych'
+                }
+            ]
         }
-    ];
+    }
 
-
-    const setValue = (startValue, maxValue) => {
-        let value = startValue < parseInt(maxValue) ? startValue + 1 : maxValue;
-        return value;
+    setValue = (startValue, maxValue) => {
+        setInterval(() => {
+            let value = startValue < parseInt(maxValue) ? startValue + 1 : maxValue;
+            return value;
+        }, 555)
     };
 
-    const items = numbersValue.map((el, key) => {
+    componentDidMount() {
+        const {boxes} = {...this.state};
+        const box1 = boxes[0];
+        const box2 = boxes[1];
+        const box3 = boxes[2];
+
+        const animatedValue = setInterval(() => {
+            if (box1['currentValue'] < parseInt(box1['value'])) box1['currentValue'] = box1['currentValue'] + 1;
+            if (box2['currentValue'] < parseFloat(box2['value'])) box2['currentValue'] = box2['currentValue'] + 0.05;
+            if (box3['currentValue'] < parseInt(box3['value'])) box3['currentValue'] = box3['currentValue'] + 5;
+            this.setState({boxes: [box1, box2, box3]});
+            if((box1['currentValue'] >= parseInt(box1['value'])) && (box2['currentValue'] >= parseFloat(box2['value'])) && (box3['currentValue'] >= parseInt(box3['value']))) clearInterval(animatedValue);
+        }, 10);
+    }
+
+    render () {
+        const items = this.state.boxes.map((el, key) => {
+            return (
+                <div key={key}>
+                    <span>{el.currentValue.toFixed(1).toString().replace(".", ",")}</span>
+                    <p>{el.title}</p>
+                </div>
+            )
+        });
+
         return (
-            <div key={key}>
-
-                <span>
-                    {setValue(0, 1000000)}
-                </span>
-
-                <p>{el.title}</p>
+            <div>
+                {items}
             </div>
         )
-    });
-
-    return (
-        <div>
-            {items}
-        </div>
-    )
+    }
 };
 
 export default NumberItems;
