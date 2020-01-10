e0e8e725fd0d394201d71811fedb74391e72fea5
bwojtal <bartekwojtal1207@gmail.com>
2020-01-10 21:15:48 +0100

    teams section

--- src/containers/homepage/Homepage.js
+++ src/containers/homepage/Homepage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import WhatWeDo from "../../components/WhatWeDo/WhatWeDo";
 import Numbers from "../../components/Numbers/Numbers";
 import Teams from "../../components/Teams/Teams";
+import JobsOffers from "../../components/JobsOffers/JobsOffers";
 
 class Homepage extends Component {
 
@@ -11,6 +12,7 @@ class Homepage extends Component {
                 <WhatWeDo/>
                 <Numbers/>
                 <Teams/>
+                <JobsOffers/>
             </div>
         )
     }
